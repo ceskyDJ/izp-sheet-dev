2fe60becb11a9f735bbea6703251b29ab60a8de1
ceskyDJ <6006391-ceskyDJ@users.noreply.gitlab.com>
2020-11-01 20:09:59 +0100

    Base for row processing

--- sheet.c
+++ sheet.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define MAX_ROW_SIZE 10 * 1024
+
+void someWorkWithRow(char row[], int rowSize);
+
+int main(int argc, char **argv) {
+    char actualRow[MAX_ROW_SIZE];
+    int i = 0;
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c != '\n') {
+            actualRow[i] = (unsigned char) c;
+
+            i++;
+        } else {
+            // TODO: Process the row...
+            someWorkWithRow(actualRow, i);
+
+            printf("\n");
+
+            // Start at the beginning of the array, old values will be replaced
+            i = 0;
+        }
+    }
 
     return 0;
+}
+
+void someWorkWithRow(char row[], int rowSize) {
+    for (int i = 0; i < rowSize; i++) {
+        printf("%c", row[i]);
+    }
 }
